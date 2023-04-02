1b1f470d34d5b2f5ed8a6a124e3c2a51d4a7d9ec
Krishanna Williams <krishannaw@gmail.com>
2023-04-02 18:57:36 -0500

    math

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
+let num1 = 2;
+let num2 = 31;
 
-// Write your code here
+let multiply = num1 * num2;
+
+console.log(multiply); // Output: 62
+let random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+
+console.log(random); // Output: random integer greater than 0
+let num3 = 14;
+let num4 = 5;
+
+let mod = num3 % num4;
+
+if (mod === 4) {
+  console.log(mod); // Output: 4
+} else {
+  console.log("The remainder is not 4.");
+}
+let numbers = [5, 12, 3, 20, 9];
+
+let max = Math.max(...numbers);
+
+console.log(max); // Output: 20
